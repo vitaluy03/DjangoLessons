ce5bd47b2cf6b1ced641365c30a3322add8be328
vitaluy03 <dv.vostoc@mail.ru>
2019-04-26 16:22:06 +0800

    s

---
+++ lesson_1/step_2/geekshop/mainapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+def catalog(request):
+    return render(request, 'mainapp/catalog.html')
+
+
+def contacts(request):
+    return render(request, 'mainapp/contacts.html')
